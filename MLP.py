bc4ce4cebfd07cb2c06a7d4af00e6087f4596d75
Joel Barrantes <joel.bg95@outlook.com>
2018-04-26 21:38:57 -0600

    Dropout

--- MLP.py
+++ MLP.py
@@ -7,13 +7,13 @@ from sklearn.model_selection import train_test_split
 mnist = fetch_mldata('MNIST original')
 train_img, test_img, train_lbl, test_lbl = train_test_split(mnist.data, mnist.target, test_size=1/7.0, random_state=0)
 
-variance = 1
-mu = 0
+
+dropout_h = 0.5
 two_layers = False
 red = NeuralNetwork(784, 100, 10, 10, 0.0085, train_img.T, train_lbl.T,test_img.T, test_lbl,two_layers )
 
-red.train_all(10000)
+#red.train_all(10000, dropout_h, dropout_i)
 
-#red.train(10000, 32)
+red.train_batch(10000, 32, dropout_h)
 
 print("Here")
--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import time
 
 class NeuralNetwork:
 
@@ -28,6 +29,8 @@ class NeuralNetwork:
         self.Y = one_hot.copy()
         self.labels = labels
 
+        self.date = time.strftime("%Y.%m.%d-%H.%M.%S")
+
         self.X_t = test_data.transpose()
         self.X_t = self.X_t / np.max(self.X_t)
         one_hot = np.zeros((t_labels.size, self.num_outputs))
@@ -46,27 +49,53 @@ class NeuralNetwork:
         d_x[d_x>0] = 1
         return f_x, d_x
 
+    def saveWs(self,W1,W2,W3):
+
+        date = self.date
+        np.savetxt("./File/W1_" + str(self.num_inputs) + "_" + str(self.num_hidden1) + "_" + date
+                   +".txt",
+                   W1,
+                   fmt = '%10.2f')
+        np.savetxt("./File/W2_" + str(self.num_hidden1) + "_" + str(self.num_hidden2) + "_" + date
+                   + ".txt",
+                   W1,
+                   fmt = '%10.2f')
+        np.savetxt("./File/W3_" + str(self.num_hidden2) + "_" + str(self.num_outputs) + "_" + date
+                   +".txt",
+                   W1,
+                   fmt = '%10.2f')
+
     def cross_entropy(self, f_w3, Y):
         loss = 1/f_w3.shape[0] * -np.sum((Y * np.log(f_w3)))
         return loss
 
-    def calculate_forward(self, X):
+    def calculate_forward(self, X, p_h):
 
         w1 = np.dot(X, self.W1)
         f_w1, d_w1 = self.ReLU(w1)
 
+        #DROPOUT HIDDEN LAYER 1
+        mask_h1 = np.random.binomial(size=f_w1.shape[1], n=1, p=p_h)/p_h
+        f_w1 = f_w1 * mask_h1
+
         if self.two_layers:
             w2 = np.dot(f_w1, self.W2)
             f_w2, d_w2 = self.ReLU(w2)
+
+            #DROPOUT HIDDEN LAYER 2
+            mask_h2 = np.random.binomial(size=f_w2.shape[1], n=1, p=p_h)/p_h
+            f_w2 = f_w2 * mask_h2
+
         else:
-            f_w2=f_w1
-            d_w2=d_w1
+            f_w2 = f_w1
+            d_w2 = d_w1
+            mask_h2 = mask_h1
         w3 = np.dot(f_w2, self.W3)
         f_w3 = self.softmax(w3)
 
-        return f_w1, f_w2, f_w3, d_w1, d_w2
+        return f_w1, f_w2, f_w3, d_w1, d_w2, mask_h1, mask_h2
 
-    def calculate_backward(self, f_w1, f_w2, f_w3, d_w1, d_w2, X, Y):
+    def calculate_backward(self, f_w1, f_w2, f_w3, d_w1, d_w2, X, Y, mh1, mh2):
 
         #CROSS ENTROPY DERIVATIVE
         deltaz3 = f_w3 - Y
@@ -76,9 +105,11 @@ class NeuralNetwork:
 
         if self.two_layers:
 
+            d_w2 = d_w2 * mh2
             deltaz2 = deltaf_w3 * d_w2
             deltaf_w2 = np.dot(deltaz2, self.W2.T)
 
+            d_w1 = d_w1 * mh1
             deltaz1 = deltaf_w2 * d_w1
             grad_w1 = np.dot(X.T, deltaz1)/ size
             grad_w2 = np.dot(f_w1.T, deltaz2)/ size
@@ -86,6 +117,7 @@ class NeuralNetwork:
 
 
         else:
+            d_w2 = d_w2 * mh2
             deltaz2 = deltaf_w3 * d_w2
             grad_w1 = np.dot(X.T, deltaz2)/ size
             grad_w2 = grad_w1
@@ -97,7 +129,7 @@ class NeuralNetwork:
         return self.cross_entropy(f_w3, Y)
 
     def calculate_accuracy_test(self, X, Y):
-        f_w1, f_w2, f_w3, d_w1, d_w2 = self.calculate_forward(X)
+        f_w1, f_w2, f_w3, d_w1, d_w2, mh1, mh2 = self.calculate_forward(X, 1)
         t = (f_w3 == f_w3.max(axis=1, keepdims=1)).astype(float)
         return np.sum(np.equal(t,Y).all(axis=1))/X.shape[0]
 
@@ -110,10 +142,11 @@ class NeuralNetwork:
 
         self.W3 = self.W3 - self.alpha * grad_w3
 
-    def train_batch(self, epochs, batch_size):
+    def train_batch(self, epochs, batch_size, p_h):
 
         idx = np.arange(self.X.shape[0])
         for j in range(0, epochs):
+            start = time.time()
             np.random.shuffle(idx)
             i=0
             Loss = 0
@@ -128,10 +161,10 @@ class NeuralNetwork:
                     batch = self.X[idx[i:i+batch_size]]
                     batch_l = self.Y[idx[i:i+batch_size]]
 
-                f_w1, f_w2, f_w3, d_w1, d_w2 = self.calculate_forward(batch)
+                f_w1, f_w2, f_w3, d_w1, d_w2, mh1, mh2 = self.calculate_forward(batch, p_h)
                 loss = self.calculate_error(f_w3, batch_l)
                 Loss += loss
-                grad_w1, grad_w2, grad_w3 = self.calculate_backward(f_w1,f_w2, f_w3, d_w1, d_w2, batch, batch_l)
+                grad_w1, grad_w2, grad_w3 = self.calculate_backward(f_w1,f_w2, f_w3, d_w1, d_w2, batch, batch_l, mh1, mh2)
 
                 self.update_weights(grad_w1, grad_w2, grad_w3)
                 i += batch_size
@@ -141,23 +174,39 @@ class NeuralNetwork:
                 if exit_cond:
                     break
 
+            self.saveWs(self.W1, self.W2, self.W3)
             acc = self.calculate_accuracy_test(self.X_t, self.Y_t)
+            end = time.time()
+
+
             print("Epoch: ", j)
             print("Loss", Loss/num_batches)
             print("Accuracy: ", acc)
+            print("Duration: ", end - start)
 
-    def train_all(self, epochs):
+
+    def train_all(self, epochs, p_h):
 
         for j in range(0, epochs):
-            f_w1, f_w2, f_w3, d_w1, d_w2 = self.calculate_forward(self.X)
+            start = time.time()
+            f_w1, f_w2, f_w3, d_w1, d_w2, mh1, mh2 = self.calculate_forward(self.X, p_h)
 
             loss = self.calculate_error(f_w3, self.Y)
             acc = self.calculate_accuracy_test(self.X_t, self.Y_t)
 
-            grad_w1, grad_w2, grad_w3 = self.calculate_backward(f_w1,f_w2, f_w3, d_w1, d_w2, self.X, self.Y)
+            grad_w1, grad_w2, grad_w3 = self.calculate_backward(f_w1,f_w2, f_w3, d_w1, d_w2, self.X, self.Y, mh1, mh2)
+
+            #DROPOUT THE GRADIENTS
+            grad_w1 = grad_w1 * mi
+            grad_w2 = grad_w2 * mh1
+            grad_w3 = grad_w3 * mh2
 
             self.update_weights(grad_w1, grad_w2, grad_w3)
 
+            self.saveWs(self.W1, self.W2, self.W3)
+
+            end = time.time()
             print("Epoch: ", j)
             print("Loss", loss)
             print("Accuracy: ", acc)
+            print("Duration: ", end - start)
