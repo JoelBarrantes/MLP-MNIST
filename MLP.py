Softmax fix

MLP.py
@@ -12,24 +12,19 @@ train_img, test_img, train_lbl, test_lbl = train_test_split(mnist.data, mnist.ta
 f_activation = 0
 
 dropout_p = 0.5
-two_layers = False
-red = NeuralNetwork(784, 100, 30, 10, 0.0085, two_layers, f_activation )
+two_layers = True
+red = NeuralNetwork(784, 32, 16, 10, 0.0085, two_layers, f_activation )
 
 
 red.load_data(train_img.T, train_lbl.T,test_img.T, test_lbl)
 #red.train_all(10000, dropout_p)cdf
 
 #LOAD PREVIOUS WEIGHTS
-w1 = np.load("./File/W1_784_100_2018.05.03-10.29.48.npy")
-w2 = np.load("./File/W2_100_30_2018.05.03-14.06.23.npy")
-w3 = np.load("./File/W3_100_10_2018.05.03-10.29.48.npy")
+w1 = np.load("./File/W1_784_32_2018.05.09-20.24.51.npy")
+w2 = np.load("./File/W2_32_16_2018.05.09-20.24.51.npy")
+w3 = np.load("./File/W3_16_10_2018.05.09-20.24.51.npy")
 
-red.load_Ws(w1,w2,w3)
+#red.load_Ws(w1,w2,w3)
 #red.no_log()
 
-red.train_batch(10000, 32, dropout_p)
-
-#do not save the weights
-
-
-print("Here")
+red.train_batch(500, 32, dropout_p)

mnist_draw.py
@@ -5,7 +5,7 @@ import scipy.misc as misc
 def save_weights(imagen,i):
     misc.imsave('./images/neuron '+i+".png", imagen)
 
-w1 = np.load("./File/W1_784_100_2018.05.03-14.06.23.npy")
+w1 = np.load("./File/W1_784_256_2018.05.10-00.49.19.npy")
 w1 = w1.T
 i = 1
 for imagen in w1:

neural_network.py
@@ -58,7 +58,8 @@ class NeuralNetwork:
         return exp_scores/np.sum(exp_scores)
 
     def softmax(self, x):
-        exp_scores = np.exp(x-np.max(x))
+        max_per_row = np.reshape(np.max(x, axis=1), (x.shape[0], 1))
+        exp_scores = np.exp(x - max_per_row)
         probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
         return probs
 
@@ -161,9 +162,11 @@ class NeuralNetwork:
 
     def update_weights(self, grad_w1, grad_w2, grad_w3):
 
+
         self.W1 = self.W1 - self.alpha * grad_w1
 
         if self.two_layers:
+
             self.W2 = self.W2 - self.alpha * grad_w2
 
         self.W3 = self.W3 - self.alpha * grad_w3